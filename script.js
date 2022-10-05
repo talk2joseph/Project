55659b9e64c2a6c9cc01f15a870f4e72c667e187
Joseph A <josephadenipekun@gmail.com>
2022-10-05 01:59:30 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function sayName() {
+    console.log('my name is Deji');
+}
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.navigation__links');
+
+hamburger.addEventListener('click', () => {
+    if (navLinks.style.transform === 'scaleX(1)') {
+        navLinks.style.transform = 'scaleX(0)';
+    } else {
+        navLinks.style.transform = 'scaleX(1)';
+    }
+});
